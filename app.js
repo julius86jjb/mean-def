83df68f2267821232fa84c824db3fa2aec8ba677
Julio Jimenez <julius.1986@gmail.com>
2019-08-30 12:20:49 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Requires
+
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables
+
+var app = express();
+
+// Conexion DB
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'online');
+});
+
+// Rutas
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    })
+
+});
+
+
+// Escuchar Peticiones
+
+app.listen(3000, () => {
+    console.log('Express Server escuchando en puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
