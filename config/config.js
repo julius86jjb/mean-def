8b4999307b2a04a96abb5fa2df7e67c3aa0e144a
Julio Jimenez <julius.1986@gmail.com>
2019-10-09 13:47:05 +0100

    variable de entorno mongo

--- config/config.js
+++ config/config.js
@@ -8,3 +8,22 @@ module.exports.CLIENT_ID = '171715616822-d5i1khoro5n7ofiee5vhpmro08d2m5h2.apps.g
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/hospitalDB';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
