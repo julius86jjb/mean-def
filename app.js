807cbe2caaf1e4b252b57926b727b0f711360434
Julio Jimenez <julius.1986@gmail.com>
2019-10-09 13:51:37 +0100

    app.js modi

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var imagenesRoutes = require('./routes/imagenes');
 
 // Conexion DB
 
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+mongoose.connection.openUri(process.env.URLDB, (err, res) => {
     if (err) throw err;
     console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'online');
 });
